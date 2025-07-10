0692380e6553061f2399fa3cdb3e7c962f79aeda
William Ton <tonthattuankiet28@gmail.com>
2025-07-09 20:45:00 -0400

    Part 1 of tutorial done

--- CPP/main.cpp
+++ CPP/main.cpp
@@ -1,7 +1,20 @@
 #include <cstdio>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <cstdint>
 
+/*************  ✨ Windsurf Command ⭐  *************/
+/**
+ * Callback function for handling GLFW errors.
+ *
+ * This function is called by GLFW whenever an error occurs. It prints
+ * the error description to the standard error stream.
+ *
+ * @param error An error code indicating the type of error that occurred.
+ * @param description A human-readable description of the error.
+ */
+
+/*******  bb0e0c8f-76ce-4300-b0d7-21120c16ccb3  *******/
 void error_callback(int error, const char* description) {
     fprintf(stderr, "Error: %s\n", description);
 }
@@ -34,4 +47,29 @@ int main() {
         glfwTerminate();
         return -1;
     }   
+
+    int glVersion[2] = {-1, 1};
+    glGetIntegerv(GL_MAJOR_VERSION, &glVersion[0]);
+    glGetIntegerv(GL_MINOR_VERSION, &glVersion[1]);
+
+    printf("Using OpenGL: %d.%d\n", glVersion[0], glVersion[1]);
+
+    glClearColor(1.0, 0.0, 0.0, 1.0);
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
 }
+
+struct Buffer
+{
+    size_t width, height;
+    uint32_t* data;
+};
+
+
